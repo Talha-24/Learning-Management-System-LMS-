92740882caef13193869998f9fcfa7b154d88a01
Mohammad Talha <talhabhatti82542@gmail.com>
2025-02-03 03:53:17 +0500

    Authentication Components are Responsive

--- src/assets/Components/Authentication/Forgot Password/Email Inquiry/Email.jsx
+++ src/assets/Components/Authentication/Forgot Password/Email Inquiry/Email.jsx
@@ -4,7 +4,7 @@ const Email = () => {
 
     <div className="w-[100%] h-[100vh] flex items-center justify-center">
       <div className="bg-[#FFFFFF] flex flex-col  rounded-[4vmin] h-[94%] w-[60%] emailparent">
-        <div className="w-[100%] text-center flex flex-col justify-center  logo">
+        <div className="w-[100%] text-center flex flex-col justify-center  elogo">
           <img src="https://s3-alpha-sig.figma.com/img/569c/113d/9b40f56e05304dfc8eada37f75b82ef1?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lcKKGr0qemFTQmzGrZ4vPY6U91ssdg98j1pKD0oeKxsDXMSJp5xEX8pWKQU2pghGx1gWHIh1D0EMLBPzRHQtANx58fkVcsBv6htjx5pNCsTg-gQ-7~Z-mH6Eu7~A7gY92WQkiyZyAOuZNaLEXG-wSv5RlBEXXUPC-TA1y6AjGRPHoTF9hfOrYqD0REQv-VekHlLFZkOvuw1lFbEWbOmi5GjtxqrXmjL6tokb~Nyeggc-NGkIUx0G-2ZAHJu7uq7YhBS~bhxQy~O7T-gnJACE-EKzhDXlVcvYCw-XxudWe6B-MSu6vY4mvjHqcC48FVDXdhnIDCQhYia4GvQcSFwm6Q__" alt="" />
           <p id='forgotpasstext' className="text-black text-[3.4vmin] font-semibold">Forgot Password</p>
         </div>
