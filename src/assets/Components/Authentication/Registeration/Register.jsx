61fcad3f9c2a4ef8e3990a1c2c8c746a66b047e1
Mohammad Talha <talhabhatti82542@gmail.com>
2025-02-03 02:54:35 +0500

    Add Image URL

--- src/assets/Components/Authentication/Registeration/Register.jsx
+++ src/assets/Components/Authentication/Registeration/Register.jsx
@@ -5,7 +5,7 @@ const Register = () => {
         <div className="w-[100%] h-[100vh] flex items-center justify-center registerparent">
             <div className="bg-[#FFFFFF] rounded-[20px] h-[92%] w-[60%] parent registerdiv">
                 <div className="w-[100%] text-center flex flex-col justify-center">
-                    <img src="src\assets\Components\Authentication\Login\image.png" alt="" />
+                    <img src="https://s3-alpha-sig.figma.com/img/569c/113d/9b40f56e05304dfc8eada37f75b82ef1?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lcKKGr0qemFTQmzGrZ4vPY6U91ssdg98j1pKD0oeKxsDXMSJp5xEX8pWKQU2pghGx1gWHIh1D0EMLBPzRHQtANx58fkVcsBv6htjx5pNCsTg-gQ-7~Z-mH6Eu7~A7gY92WQkiyZyAOuZNaLEXG-wSv5RlBEXXUPC-TA1y6AjGRPHoTF9hfOrYqD0REQv-VekHlLFZkOvuw1lFbEWbOmi5GjtxqrXmjL6tokb~Nyeggc-NGkIUx0G-2ZAHJu7uq7YhBS~bhxQy~O7T-gnJACE-EKzhDXlVcvYCw-XxudWe6B-MSu6vY4mvjHqcC48FVDXdhnIDCQhYia4GvQcSFwm6Q__" alt="" />
                     <p id='signuptxt' className="text-black text-[3.2vmin]">Signup Your Account</p>
                 </div>
                 <div className="Reginputs">
