ef9d4a300b719d77193ff28ee6e8e14d6ef2c9f0
Mohammad Talha <talhabhatti82542@gmail.com>
2025-02-03 02:58:37 +0500

    Add BackgroundColor for Component, to show Logo Text

--- src/assets/Components/Authentication/Forgot Password/OTP/Otp.jsx
+++ src/assets/Components/Authentication/Forgot Password/OTP/Otp.jsx
@@ -3,7 +3,7 @@ import "./OTP.css";
 const Otp = () => {
   return (
     <div className="w-[100%] h-[100vh] flex items-center justify-center otpparent">
-    <div id="otpdiv" className="bg-[#FFFFFF] flex flex-col gap-[7vmin] items-center  rounded-[2vmin] h-[92%] w-[60%] parent">
+    <div id="otpdiv" className="bg-[#FAF9F6] flex flex-col gap-[7vmin] items-center  rounded-[2vmin] h-[92%] w-[60%] parent">
           <img src="https://s3-alpha-sig.figma.com/img/569c/113d/9b40f56e05304dfc8eada37f75b82ef1?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lcKKGr0qemFTQmzGrZ4vPY6U91ssdg98j1pKD0oeKxsDXMSJp5xEX8pWKQU2pghGx1gWHIh1D0EMLBPzRHQtANx58fkVcsBv6htjx5pNCsTg-gQ-7~Z-mH6Eu7~A7gY92WQkiyZyAOuZNaLEXG-wSv5RlBEXXUPC-TA1y6AjGRPHoTF9hfOrYqD0REQv-VekHlLFZkOvuw1lFbEWbOmi5GjtxqrXmjL6tokb~Nyeggc-NGkIUx0G-2ZAHJu7uq7YhBS~bhxQy~O7T-gnJACE-EKzhDXlVcvYCw-XxudWe6B-MSu6vY4mvjHqcC48FVDXdhnIDCQhYia4GvQcSFwm6Q__" alt="" />
     <div className='w-[100%] flex flex-col gap-[2vmin]  items-center'>
         <p className='otpemailtext'>Send OTP on Your Email</p>
